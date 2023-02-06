3abfcd9b90a8e5a94ddba9ec0fbb394e689513c8
OmarinaA <marina.missis.omochka@gmail.com>
2023-02-06 18:49:01 +0300

    Добавила функционал

--- lib/CostCalculator.dart
+++ lib/CostCalculator.dart
@@ -29,89 +29,87 @@ class _CostCalculatorState extends State<CostCalculator> {
     }
 
     return Scaffold(
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.only(bottom: 40),
-        // child: Center(
-        //   child: Container(
-        //   width: 700.0,
-        //   color: Colors.white,
-            child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Divider(
-              color: Color(0xffD9D9D9),
-              thickness: 2,
-              indent: 5,
-              endIndent: 5,
-              height: 1,
+        bottomNavigationBar: Padding(
+          padding: EdgeInsets.only(bottom: 40),
+          // child: Center(
+          //   child: Container(
+          //   width: 700.0,
+          //   color: Colors.white,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Divider(
+                color: Color(0xffD9D9D9),
+                thickness: 2,
+                indent: 5,
+                endIndent: 5,
+                height: 1,
+              ),
+              LisTileExample(
+                price: sum,
+                title: 'Общая стоимость',
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('button pressed!');
+                },
+                child: Text('ОПЛАТИТЬ'),
+              ),
+            ],
+          ),
+        ),
+        body: ListView(
+          padding: EdgeInsets.only(left: 60, top: 50),
+          children: <Widget>[
+            Text('Калькулятор стоимости продления'.toUpperCase(),
+                textAlign: TextAlign.left,
+                style: Theme.of(context).textTheme.titleLarge),
+            Padding(
+              padding: EdgeInsets.only(bottom: 40, top: 20),
+              child: Text('Выберите функции, которые вам необходимы',
+                  textAlign: TextAlign.left,
+                  style: Theme.of(context).textTheme.titleMedium),
             ),
             LisTileExample(
-              price: sum,
-              title: 'Общая стоимость',
+              price: basicTariff.price,
+              title: basicTariff.title,
+              description:
+                  '* ### Общий функционал \nЛичный кабинет, Сообщества, Трекер активности, Социальная сеть, Геймификация \n* ### Дизайн \n7 цветов \n* ### Интеграции \n1С, Крафт, Клабис, Юниверс, A&A, МФ Расписание, Запись на уроки, Запись к тренеру, Личный кабинет Настройка платежной системы (3 варианта)\n* ### Рассылки \n* ### Поддержка \nЧат со службой заботы',
+              isSelected: basicTariff.isSelected,
             ),
-            ElevatedButton(
-              onPressed: () {
-                print('button pressed!');
-              },
-              child: Text('ОПЛАТИТЬ'),
-          ),
+            LisTileExample(
+                price: customDesignLogo.price,
+                title: customDesignLogo.title,
+                description: '',
+                isSelected: customDesignLogo.isSelected,
+                onChanged: (value) {
+                  onChanged(customDesignLogo, value);
+                }),
+            LisTileExample(
+                price: scheduleWidget.price,
+                title: scheduleWidget.title,
+                description: '',
+                isSelected: scheduleWidget.isSelected,
+                onChanged: (value) {
+                  onChanged(scheduleWidget, value);
+                }),
+            LisTileExample(
+                price: analytics.price,
+                title: analytics.title,
+                description: '',
+                isSelected: analytics.isSelected,
+                onChanged: (value) {
+                  onChanged(analytics, value);
+                }),
+            LisTileExample(
+                price: uploadOnlineWorkouts.price,
+                title: uploadOnlineWorkouts.title,
+                description: '',
+                isSelected: uploadOnlineWorkouts.isSelected,
+                onChanged: (value) {
+                  onChanged(uploadOnlineWorkouts, value);
+                }),
           ],
-        ),
-      ),
-      body: ListView(
-        padding: EdgeInsets.only(left: 60, top: 50),
-          children: <Widget>[
-          Text('Калькулятор стоимости продления'.toUpperCase(),
-              textAlign: TextAlign.left,
-              style: Theme.of(context).textTheme.titleLarge),
-          Padding(
-            padding: EdgeInsets.only(bottom: 40, top: 20),
-            child: Text('Выберите функции, которые вам необходимы',
-                textAlign: TextAlign.left,
-                style: Theme.of(context).textTheme.titleMedium),
-          ),
-          LisTileExample(
-            price: basicTariff.price,
-            title: basicTariff.title,
-            description:
-                '* ### Общий функционал \nЛичный кабинет, Сообщества, Трекер активности, Социальная сеть, Геймификация \n* ### Дизайн \n7 цветов \n* ### Интеграции \n1С, Крафт, Клабис, Юниверс, A&A, МФ Расписание, Запись на уроки, Запись к тренеру, Личный кабинет Настройка платежной системы (3 варианта)\n* ### Рассылки \n* ### Поддержка \nЧат со службой заботы',
-            isSelected: basicTariff.isSelected,
-          ),
-          LisTileExample(
-              price: customDesignLogo.price,
-              title: customDesignLogo.title,
-              description: '',
-              isSelected: customDesignLogo.isSelected,
-              onChanged: (value) {
-                onChanged(customDesignLogo, value);
-              }),
-          LisTileExample(
-              price: scheduleWidget.price,
-              title: scheduleWidget.title,
-              description: '',
-              isSelected: scheduleWidget.isSelected,
-              onChanged: (value) {
-                onChanged(scheduleWidget, value);
-              }),
-          LisTileExample(
-              price: analytics.price,
-              title: analytics.title,
-              description: '',
-              isSelected: analytics.isSelected,
-              onChanged: (value) {
-                onChanged(analytics, value);
-              }),
-          LisTileExample(
-              price: uploadOnlineWorkouts.price,
-              title: uploadOnlineWorkouts.title,
-              description: '',
-              isSelected: uploadOnlineWorkouts.isSelected,
-              onChanged: (value) {
-                onChanged(uploadOnlineWorkouts, value);
-              }),
-        ],
-      )
-    );
-    
+        ));
   }
 }
